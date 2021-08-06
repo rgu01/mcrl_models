127ea8feb064290ceb8c8f73f6f11a40b87156dd
rgu01 <39125216+rgu01@users.noreply.github.com>
2021-08-06 11:14:06 +0200

    Update experiment-8agents.sh

--- experiment-8agents.sh
+++ experiment-8agents.sh
@@ -1,3 +1,2 @@
 #
-#UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/models/mcrl_normal.so  ../uppaal-stratego-beta-v3/bin-Linux/verifyta -u ~/uppaal/models/4agents/learning-4agents-uncertain.xml -D 0.01 --learning-method -1 --seed=42 -s --max-iterations 20 --eval-runs 20 --good-runs 8000 --total-runs 8000 --reset-no-better 5 --max-reset-learning 3 >> results/normal4
-UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/uppaal/models/mcrl_nn.so  ../uppaal-stratego-beta-v3/bin-Linux/verifyta -u ~/uppaal/models/8agents/learning-8agents.xml -D 0.01 --learning-method -1 -s --max-iterations 20 --eval-runs 20 --good-runs 15000 --total-runs 15000 --reset-no-better 5 --max-reset-learning 3 >> results/nn8
+UPPAAL_EXTERNAL_LEARNER_LIBRARY=~/Rong/mcrl_models/mcrl_nn.so  ../stratego/verifyta -u ~/Rong/mcrl_models/8agents/learning-8agents.xml -D 0.01 --learning-method -1 -s --max-iterations 20 --eval-runs 20 --good-runs 15000 --total-runs 15000 --reset-no-better 5 --max-reset-learning 3 >> results/nn8
